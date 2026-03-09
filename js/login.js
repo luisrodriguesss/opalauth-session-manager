d75f05ad8d75b4dce32cc29bfeefba7bd9cdb730
Kayque2012 <kayquedr2012@gmail.com>
2026-03-09 11:30:13 -0300

    feat: conversao de tipos e captura do campo senha.

---
+++ js/login.js
@@ -0,0 +1,19 @@
+
+const botaoEnviar = document.querySelector("#btn-entrar")
+
+//botaoEnviar.addEventListener( "click", function(){
+//    console.log()
+// } );
+
+
+function CampoValida(){
+    //capturando os dados do campo SENHA
+const CampoSenha = document.getElementById("Input-senha");
+let CampoValor = parseInt(CampoSenha.value);
+
+
+//Verificando se a senha é PAR
+console.log(CampoValor % 2 == 0)
+}
+
+botaoEnviar.addEventListener( "click", CampoValida());
